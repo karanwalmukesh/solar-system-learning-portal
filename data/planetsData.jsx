1a9ba65044a12b21354225d892ed59f667a2bb10
karanwalmukesh <karanwal.mukesh@gmail.com>
2026-06-01 16:14:14 +0530

    Add files via upload

---
+++ data/planetsData.jsx
@@ -0,0 +1,91 @@
+// src/data/planetsData.js
+export const planetsData = [
+  {
+    name: 'Mercury',
+    size: 0.4,
+    distance: 5.5,
+    speed: 0.04,
+    color: '#D4A574',
+    orbitColor: '#E8D4B8',
+    labelColor: '#FFD700',
+    funFact: 'Mercury is the smallest and fastest planet!',
+    description: 'Closest planet to the Sun'
+  },
+  {
+    name: 'Venus',
+    size: 0.5,
+    distance: 7.5,
+    speed: 0.025,
+    color: '#E6B800',
+    orbitColor: '#FFD700',
+    labelColor: '#FFE4B5',
+    funFact: 'Venus spins backwards compared to other planets!',
+    description: 'Earth\'s twin sister'
+  },
+  {
+    name: 'Earth',
+    size: 0.55,
+    distance: 9.5,
+    speed: 0.02,
+    color: '#4A90E2',
+    orbitColor: '#6BB5FF',
+    labelColor: '#87CEEB',
+    funFact: 'Our home! The only planet with life we know!',
+    description: 'The Blue Planet'
+  },
+  {
+    name: 'Mars',
+    size: 0.5,
+    distance: 11.5,
+    speed: 0.017,
+    color: '#E85D3A',
+    orbitColor: '#FF8C5A',
+    labelColor: '#FFA07A',
+    funFact: 'Has the tallest volcano in the solar system!',
+    description: 'The Red Planet'
+  },
+  {
+    name: 'Jupiter',
+    size: 1.2,
+    distance: 14.5,
+    speed: 0.01,
+    color: '#D4A574',
+    orbitColor: '#C9A06C',
+    labelColor: '#F5DEB3',
+    funFact: 'The biggest planet! Could fit 1,300 Earths inside!',
+    description: 'King of Planets'
+  },
+  {
+    name: 'Saturn',
+    size: 1.0,
+    distance: 17.5,
+    speed: 0.008,
+    color: '#F4D4A0',
+    orbitColor: '#E8C89A',
+    labelColor: '#FFF0D4',
+    funFact: 'Famous for its beautiful rings!',
+    description: 'The Ringed Planet'
+  },
+  {
+    name: 'Uranus',
+    size: 0.8,
+    distance: 20.5,
+    speed: 0.006,
+    color: '#B0E0E6',
+    orbitColor: '#90D0DA',
+    labelColor: '#C8F0F0',
+    funFact: 'Rolls on its side as it orbits!',
+    description: 'The Sideways Planet'
+  },
+  {
+    name: 'Neptune',
+    size: 0.8,
+    distance: 23.5,
+    speed: 0.005,
+    color: '#4169E1',
+    orbitColor: '#6B8FE5',
+    labelColor: '#9BB9FF',
+    funFact: 'Has the strongest winds in the solar system!',
+    description: 'The Windy Planet'
+  }
+]
